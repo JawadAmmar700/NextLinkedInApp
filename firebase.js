a4eebb9b29a6d9376734416ef31f7b82474c2365
jawadGitNext <jawadGitNext2021@gmail.com>
2021-09-30 17:59:40 +0300

    intial

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-import firebase from 'firebase'
+import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdbWJ2lbdYpPEPv0tkOWCJ-ZRjmjhRLvA",
-    authDomain: "linkedin-50daa.firebaseapp.com",
-    projectId: "linkedin-50daa",
-    storageBucket: "linkedin-50daa.appspot.com",
-    messagingSenderId: "745143700117",
-    appId: "1:745143700117:web:1024046c1caa8fed47299c",
-    measurementId: "G-P4F85YJDZH"
-};
+  apiKey: "AIzaSyDODgjCbxlioxLpba2ev38Z9ztSX7Lo6t0",
+  authDomain: "next-linkedin-34f99.firebaseapp.com",
+  projectId: "next-linkedin-34f99",
+  storageBucket: "next-linkedin-34f99.appspot.com",
+  messagingSenderId: "788364234992",
+  appId: "1:788364234992:web:89e023a5c132c1ff083235",
+  measurementId: "G-KXPBY0T6XX",
+}
 
 if (!firebase.apps.length) {
-
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
 }
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
 
-
-export { auth, provider };
+export { auth, provider }
